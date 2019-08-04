4d45ea82e1ac9b84ff4df2e0643c0ced7bdc69b3
Simon Kast <simon.kast@hotmail.com>
2019-08-04 17:06:03 +0200

    prepare the project for unit testing

---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Library.UnitTests/LoginViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using ZbW.Testing.Dms.Client.Views;
+
+namespace ZbW.Testing.Dms.Library.UnitTests
+{
+    using NUnit.Framework;
+    using ZbW.Testing.Dms.Client.ViewModels;
+
+    [TestFixture]
+    class LoginViewModelTests
+    {
+
+        [Test]
+        public void OnCanLogin_ValideUserName_RetrunsTrue()
+        {
+            // arrange
+            var loginViewModel = new LoginViewModel(new LoginView());
+
+            // act
+            var result = loginViewModel.CmdLogin.CanExecute();
+
+            // assert
+            Assert.That(result, Is.True);
+        }
+
+    }
+}
