203371d58afe15868eb69df1e40d1f7603c351b8
Simon Kast <simon.kast@hotmail.com>
2019-08-11 15:59:53 +0200

    add forgotten files

---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Library.InterfaceTests/MetadataItemTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+using ZbW.Testing.Dms.Client.TestableObjects;
+
+namespace ZbW.Testing.Dms.Library.InterfaceTests
+{
+    using NUnit.Framework;
+    using ZbW.Testing.Dms.Client.Model;
+
+    [TestFixture]
+    class MetadataItemTests
+    {
+        [Test] public void ValideMetadata_CheckDependency_TestableMessageBoxCalled()
+        { 
+            // arrange
+            var metadataItem = new MetadataItem("Simon",DateTime.Now,"","");
+            var testableMessageBoxMock = A.Fake<TestableMessageBox>();
+            // act
+            var result = metadataItem.ValideMetadata(testableMessageBoxMock);
+
+            // assert
+            A.CallTo(() => testableMessageBoxMock.Show("Es müssen alle Pflichtfelder ausgefüllt werden")).MustHaveHappened();
+        }
+
+       
+
+      
+
+
+    }
+}
